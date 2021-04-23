eb7c721e42f6da1983b9467017abdfcae6eab435
Abdulrahman_Aljafar <abdulrahmana.jafar@gmail.com>
2021-04-24 01:45:55 +0300

    Update program.cs

--- Program.cs
+++ Program.cs
@@ -9,9 +9,19 @@ namespace JsonParserCSharp
     {
         static void Main(string[] args)
         {
-            Tokenizer t = new Tokenizer(new Input("{}"), new Tokenizable[] {
-
-            }); ; Token token = t.tokenize();
+            Tokenizer t = new Tokenizer(new Input("5.5 True null False -5.5e+5 5e+2 -5e+2 5e5 -5e5 5.5E+5 -5.5E+5 5E+2 -5E+2 5E5 -5E5  "), new Tokenizable[] {
+                new NumberTokenizer(),
+                new WhiteSpaceTokenizer(),
+                new boolTokenizer(new List<string>
+                {
+                    "true","false"
+                }),
+                new nullTokenizer(new List<string>
+                {
+                    "null"
+                })
+            }) ; 
+            Token token = t.tokenize();
             while (token != null)
             {
                 Console.WriteLine(token.Value);
