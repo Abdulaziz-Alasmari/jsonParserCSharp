985b0376ba26a2206605c22610f15d2adc96f29f
Ahmed Alzubaidi <Ahmed.Alzubaidi@tuwaiq.edu.sa>
2021-04-24 06:42:49 +0300

    fixed: some bugs in StringTokenizer

--- StringTokenizer.cs
+++ StringTokenizer.cs
@@ -8,28 +8,37 @@ namespace JsonParserCSharp
 {
     class StringTokenizer : Tokenizable
     {
+        private static int count = 0;
         private string valid = ",:]}";
 
         public override bool tokenizable(Tokenizer tokenizer)
         {
+            count = 0;
             return tokenizer.input.peek() == '"';
         }
 
         public bool isString(Input input)
         {
+            count++;
+
             bool endWithoutClosing = !input.hasMore(2) && input.peek() != '"';
             bool nextTokenInvalid = input.peek() == '"' && input.Character != '\\' && !valid.Contains(input.peek(2));
 
             if (input.peek() == '\n' || endWithoutClosing || nextTokenInvalid)
                 throw new Exception("Unexpected token");
 
-            return input.Character != '"';
+            return count == 1 || input.Character != '"';
         }
 
         public override Token tokenize(Tokenizer tokenizer)
         {
             Input input = tokenizer.input;
-            return new Token(input.Position, input.LineNumber, "type", input.loop(isString));
+            char ch = input.step().Character;
+            Token token = new Token(input.Position, input.LineNumber, "type", input.loop(isString));
+            token.Value = ch + token.Value;
+            Console.WriteLine(token.Value);
+
+            return token;
         }
     }
 }
