8e4b1b1f4a59906f70eb111d86d3975d1b5db2c0
Ahmed Alzubaidi <Ahmed.Alzubaidi@tuwaiq.edu.sa>
2021-04-25 00:54:07 +0300

    refactor: shortened ToString code

--- JsonArr.cs
+++ JsonArr.cs
@@ -6,20 +6,9 @@ namespace JsonParserCSharp
     {
         public List<JsonValue> value = new List<JsonValue>();
 
-        public override string ToString( )
+        public override string ToString()
         {
-            string stringArr = "[";
-            for (var i = 0; i < value.Count ; i++)
-            {
-                if( (i + 1) == value.Count )
-                {
-                    stringArr += value[i].ToString();
-                    break;
-                }
-                stringArr += value[i].ToString() + ", ";
-            }
-            stringArr += "]";
-            return stringArr;
+            return $"[{string.Join(", ", value)}]";
         }
     }
 }
